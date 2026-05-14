a42f91d88de3a9feaa151101d6c9ca1179b9195e
NormalGuy Dasu <130691655+NormalGuy-Dasu@users.noreply.github.com>
2026-05-14 15:22:24 +0700

    update module 2: Su dung vietOCR GPU

--- module_2_core_ocr/config.py
+++ module_2_core_ocr/config.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 class OcrConfig:
     lang: str = "en"
     use_angle_cls: bool = True
-    use_gpu: bool = False
+    use_gpu: bool = True
     
     # True: Nếu Đạt đang test folder chứa toàn ảnh Scan/Digital
     # False: Nếu Đạt đang test folder chứa ảnh chụp camera
